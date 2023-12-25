d4c7be8211753536c8188a90add983b89f6e1bf9
Harsh Sharma <harshsharma20503@gmail.com>
2023-12-25 14:53:30 +0530

    first commit

---
+++ app/src/main/java/com/xoxoharsh/notesapp/SplashActivity.java
@@ -0,0 +1,33 @@
+package com.xoxoharsh.notesapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+@SuppressLint("CustomSplashScreen")
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(() -> {
+            FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+            if(currentUser == null)
+            {
+                startActivity(new Intent(SplashActivity.this,LoginActivity.class));
+            }
+            else {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+            }
+            finish();
+        }, 1000);
+    }
+}
